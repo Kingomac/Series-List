11a519970e7b0c7ce23be54db001c6ee5398163e
Izumu-san <kingomacyt@gmail.com>
2020-04-25 10:21:20 +0200

    Don't ignore config to allow deploy with actions

---
+++ app.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  firebase_config: {
+    apiKey: "AIzaSyBF3Q8IMzrcBjPROUKjPfaVq0ZiQfOnCPo",
+    authDomain: "what-anime-i-see.firebaseapp.com",
+    databaseURL: "https://what-anime-i-see.firebaseio.com",
+    projectId: "what-anime-i-see",
+    storageBucket: "what-anime-i-see.appspot.com",
+    messagingSenderId: "239560464048",
+    appId: "1:239560464048:web:5f6e2c6391780d896e0d75"
+  }
+}
