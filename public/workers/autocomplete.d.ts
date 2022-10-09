f4b526189c785397095e5e13916d226c121f327f
mariovlcm <vcmario3@gmail.com>
2022-10-09 08:57:53 +0200

    Autocomplete improved and dependency update

---
+++ public/workers/autocomplete.d.ts
@@ -0,0 +1,3 @@
+declare function searchStart(term: string, input: string[]): Promise<number>;
+declare function searchEnd(term: string, input: string[]): Promise<number>;
+declare function searchStartWith(term: string, input: string[]): Promise<string[]>;
--- public/workers/autocomplete.js
+++ public/workers/autocomplete.js
@@ -1,3 +1,39 @@
+'use strict'
+
+/**
+ *
+ * @param {string} term
+ * @param {string[]} input
+ * @returns
+ */
+async function searchStart (term, input) {
+  let start = 0
+  while (!input[start].toLocaleLowerCase().startsWith(term)) {
+    start++
+    console.log(input)
+    console.log(start)
+    console.log(input[start])
+  }
+  return start
+}
+
+/**
+ *
+ * @param {string} term
+ * @param {string[]} input
+ * @returns
+ */
+async function searchEnd (term, input) {
+  let end = input.length - 1
+  while (end > 0 && !input[end].toLocaleLowerCase().startsWith(term)) {
+    end--
+  }
+  return end
+}
+async function searchStartWith (term, input) {
+  const results = await Promise.all([searchStart(term, input), searchEnd(term, input)])
+  return input.slice(results[0], results[1] + 1)
+}
 
 self.onmessage = async (msg) => {
   /** @type string */
@@ -6,15 +42,10 @@ self.onmessage = async (msg) => {
   const link = `/assets/anime_completions/${firstLet.toLowerCase()}.txt`
   if (link === undefined) {
     postMessage('sadge')
-  } else {
-    const req = await fetch(link)
-    const res = await req.text()
-    const toret = []
-    for await (const line of res.split(/\r?\n/)) {
-      if (line.toLowerCase().startsWith(term.toLowerCase())) {
-        toret.push(line)
-      }
-    }
-    postMessage(toret)
+    return
   }
+  const req = await fetch(link)
+  const res = await req.text()
+  const toret = await searchStartWith(term.toLocaleLowerCase(), res.split(/\r?\n|\r|\n/g))
+  postMessage(toret)
 }
